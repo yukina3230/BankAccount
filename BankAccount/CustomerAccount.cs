d470dfab5d4aeb40a88f4d1842fd399e34e800b6
Velvet <Velvet@LAPTOP-0PJVBT4K>
2023-08-30 22:09:42 +0700

    Add project files.

---
+++ BankAccount/CustomerAccount.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    public class CustomerAccount
+    {
+        public string CustomerID { get; set; }
+        public string CustomerAccountName { get; set; }
+        public decimal InitialFund { get; set; }
+
+        public CustomerAccount(string aCustomerID,string aCustomerAccountName,decimal aInitialFund) 
+        {
+            CustomerID = aCustomerID;
+            CustomerAccountName = aCustomerAccountName;
+            InitialFund = aInitialFund = 10000;
+        }
+        
+
+    }
+}
---
+++ BankAccount/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.ComponentModel;
+using System.Security.Cryptography;
+
+namespace BankAccount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //UniqueNumber will be added as string to ID and increase by 1 each time an account created, avoid duplicate ID
+            int UniqueNumber = 1000;
+            string CustomerID = "";
+            string CustomerAccountName = "";
+            decimal InitialFund = 0;
+
+            //First time Account 
+            MenuOption.CreateAccount(ref CustomerID, ref CustomerAccountName, ref InitialFund, ref UniqueNumber);
+            MenuOption.AccountInfo(CustomerID, CustomerAccountName, InitialFund);
+            MenuScreen(ref CustomerAccountName, ref CustomerID, ref InitialFund, ref UniqueNumber);
+
+        }
+
+        static void MenuScreen(ref string CustomerAccountName, ref string CustomerID, ref decimal InitialFund, ref int i)
+        {
+            Console.WriteLine("Please pick your option: 1.Create New Account;2.Add Fund;3.Withdraw Fund;4.Check Account Info");
+            int OptionNumb = Convert.ToInt32(Console.ReadLine());
+            switch (OptionNumb)
+            {
+                case 1://Create Account
+
+                    MenuOption.CreateAccount(ref CustomerID, ref CustomerAccountName, ref InitialFund, ref i);
+                    MenuOption.AccountInfo(CustomerID, CustomerAccountName, InitialFund);
+                    MenuScreen(ref CustomerID, ref CustomerAccountName, ref InitialFund, ref i);
+                    break;
+                case 2://Add Fund
+                    MenuOption.AddFund(ref InitialFund);
+                    MenuOption.AccountInfo(CustomerID, CustomerAccountName, InitialFund);
+                    MenuScreen(ref CustomerID, ref CustomerAccountName, ref InitialFund, ref i);
+                    break;
+                case 3://Withdraw Fund
+                    MenuOption.WithdrawFund(ref InitialFund);
+                    MenuOption.AccountInfo(CustomerID, CustomerAccountName, InitialFund);
+                    MenuScreen(ref CustomerID, ref CustomerAccountName, ref InitialFund, ref i);
+                    break;
+                case 4://Check Account Info
+                    MenuOption.AccountInfo(CustomerID, CustomerAccountName, InitialFund);
+                    MenuScreen(ref CustomerID, ref CustomerAccountName, ref InitialFund, ref i);
+                    break;
+            }
+
+
+        }
+    }
+}
