e11b8ef289769c570506493f547d67d8b0ea4614
yukina3230 <thienphuc3230@gmail.com>
2023-08-31 09:09:53 +0700

    update customer services

--- .history/BankAccount/CustomerService_20230831082152.cs
+++ .history/BankAccount/CustomerService_20230831082152.cs
@@ -26,9 +26,7 @@ public class CustomerService
     private void MenuScreen()
     {
         Console.Write("Please pick your option: \n1. Create New Account \n2. Add Fund \n3. Withdraw Fund \n4. Check Account Info \nPick: ");
-        int option;
-        bool isNumber = Int32.TryParse(Console.ReadLine(), out option); // To avoid entering characters
-        if (!isNumber) option = 0;
+        int option = int.Parse(Console.ReadLine());
         switch (option)
         {
             case 1: // Create Account
@@ -56,21 +54,19 @@ public class CustomerService
         custAccount = new CustomerAccount(); // Clear old data
         while (true)
         {
-            Console.WriteLine("\n-- CREATE --");
             Console.Write("Please enter your Account name: ");
             custAccount.CustomerAccountName = Console.ReadLine()?.Trim();
 
             if (custAccount.CustomerAccountName?.Length < 9)
             {
                 Console.Write("> ERROR: \"Account name must be more than 9 letters! Please try again.\"");
-                Console.ReadKey(); Console.WriteLine();
             }
             else
             {
-                Console.WriteLine("Account created successfully!");
-                Console.Write($"Your ID is: {uniqueID}.");
-                custAccount.CustomerID = uniqueID; id = newID;
-                Console.ReadKey(); Run(); break;
+                Console.WriteLine("Account created successfully");
+                Console.Write($"Your ID is: {uniqueID}");
+                custAccount.CustomerID = uniqueID;
+                id = newID; Console.ReadKey(); Run(); break;
             }
         }
     }
@@ -80,21 +76,14 @@ public class CustomerService
         CheckAccountExist();
         while (true)
         {
-            Console.WriteLine("\n-- TOP UP --");
             Console.Write("Please enter the Amount: ");
             double newAmount = Convert.ToDouble(Console.ReadLine());
             if (newAmount < 10000 || newAmount > 50000)
             //Customer can only Add between 1000 and 50000
             {
-                Console.Write("> ERROR: \"The Amount have to be between 10000 and 50000!\"");
-                Console.ReadKey(); Console.WriteLine();
+                Console.Write("> ERROR: \"The Amount have to be between 10000 and 50000\"");
             }
-            else
-            {
-                custAccount.InitialFund += newAmount;
-                Console.WriteLine($"{newAmount} added to your account!\nYour balance is {custAccount.InitialFund}.");
-                Console.ReadKey(); Run(); break;
-            }
+            else custAccount.InitialFund += newAmount; Run(); break;
         }
     }
 
@@ -103,30 +92,14 @@ public class CustomerService
         CheckAccountExist();
         while (true)
         {
-            Console.WriteLine("\n-- WITHDRAW --");
             Console.Write("Please enter the Amount: ");
             double withdrawAmount = Convert.ToDouble(Console.ReadLine());
-
-            // Customer cannot withdraw more than the balance
-            if (withdrawAmount > custAccount.InitialFund)
+            if (withdrawAmount < 10000 || withdrawAmount > 50000 || withdrawAmount > custAccount.InitialFund)
+            //Customer can only Withdraw the money they have and it have to be between 10000 and 50000
             {
-                Console.Write("> ERROR: \"You do not have enough money to withdraw!\"");
-                Console.ReadKey(); Console.WriteLine(); Run();
-                continue;
+                Console.Write("> ERROR: \"The Withdraw Amount have to be between 10000 and 50000\"");
             }
-
-            // Customer can only Withdraw the money they have and it have to be between 10000 and 50000
-            if (withdrawAmount < 10000 || withdrawAmount > 50000)
-            {
-                Console.Write("> ERROR: \"The Withdraw Amount have to be between 10000 and 50000!\"");
-                Console.ReadKey(); Console.WriteLine();
-            }
-            else
-            {
-                custAccount.InitialFund -= withdrawAmount;
-                Console.WriteLine($"You withdrew {withdrawAmount}!\nYour balance is {custAccount.InitialFund}.");
-                Console.ReadKey(); Run(); break;
-            }
+            else custAccount.InitialFund -= withdrawAmount; Run(); break;
         }
     }
 
